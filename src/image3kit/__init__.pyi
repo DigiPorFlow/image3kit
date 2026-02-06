aliraeini/image3d

mass_commit: refactorings, bugfixes and exposing U16... voxelImage types to Python

src/image3d/__init__.pyi
@@ -3,12 +3,14 @@ The _core (PyBind11 wrapper) of image3 module of containing sirun and VxlImg sub
 """
 from __future__ import annotations
 from image3d._core.sirun import Input
+from image3d._core.voxlib import VxlImgF32
+from image3d._core.voxlib import VxlImgI32
+from image3d._core.voxlib import VxlImgU16
 from image3d._core.voxlib import VxlImgU8
 from image3d._core.voxlib import cube
 from image3d._core.voxlib import cylinder
-from image3d._core.voxlib import readImage
-from image3d._core.voxlib import readImageU8
+from image3d._core.voxlib import readImageBase
 from image3d._core.voxlib import sphere
 from . import _core
-__all__: list = ['__doc__', '__version__', 'VxlImgU8', 'Input', 'sphere', 'cylinder', 'cube', 'readImage', 'readImageU8']
+__all__: list = ['__doc__', '__version__', 'VxlImgU8', 'VxlImgU16', 'VxlImgI32', 'VxlImgF32', 'Input', 'cube', 'cylinder', 'sphere', 'readImageBase']
 __version__: str = '0.0.1'

tests/test_2to3d.py
@@ -2,19 +2,27 @@ from __future__ import annotations
 
 from pathlib import Path
 
-import image3d as im
+import image3d as c3d
 
 cwd = Path(__file__).parent
+
 def test_readPng():
-    img = im.VxlImgU8()
+    img = c3d.VxlImgU8()
     assert img.data().shape == (0,0,0)
-    img = im.readImage(cwd / "piskelapp.png")
-    assert img.shape() == img.data().shape
-    assert img.shape() == img.data().shape
-    img.plotAll()
+    img = c3d.VxlImgU8(cwd / "piskelapp.png")
     print(img, img.data().shape)
+    img.printInfo()
+    assert img.shape() == img.data().shape
+    assert img.nz() == 1
+    img.plotAll(zProfile=False) # FIXME: zProfile hangs svplot when nz==1
+    img.distMapExtrude(offset=0.5, scale=2.0,)
+    assert img.nz() > 1
+    slice=8
+    img.plotAll(name="extruded", normalAxis="z", sliceIndex=slice)
+    assert Path(f"fig/extruded_z{slice}_grey.png").exists()
 
 if __name__ == "__main__":
     # test_version()
     # test_voxlibI()
     test_readPng()
+ 

DigiPorFlow/image3kit

src/image3kit/__init__.pyi
@@ -3,12 +3,14 @@ The _core (PyBind11 wrapper) of image3 module of containing sirun and VxlImg sub
 """
 from __future__ import annotations
 from image3kit._core.sirun import Input
+from image3kit._core.voxlib import VxlImgF32
+from image3kit._core.voxlib import VxlImgI32
+from image3kit._core.voxlib import VxlImgU16
 from image3kit._core.voxlib import VxlImgU8
 from image3kit._core.voxlib import cube
 from image3kit._core.voxlib import cylinder
-from image3kit._core.voxlib import readImage
-from image3kit._core.voxlib import readImageU8
+from image3kit._core.voxlib import readImageBase
 from image3kit._core.voxlib import sphere
 from . import _core
-__all__: list = ['__doc__', '__version__', 'VxlImgU8', 'Input', 'sphere', 'cylinder', 'cube', 'readImage', 'readImageU8']
+__all__: list = ['__doc__', '__version__', 'VxlImgU8', 'VxlImgU16', 'VxlImgI32', 'VxlImgF32', 'Input', 'cube', 'cylinder', 'sphere', 'readImageBase']
 __version__: str = '0.0.1'

tests/test_2to3d.py
@@ -5,14 +5,21 @@ from pathlib import Path
 import image3kit as ik
 
 cwd = Path(__file__).parent
+
 def test_readPng():
     img = ik.VxlImgU8()
     assert img.data().shape == (0,0,0)
-    img = ik.readImage(cwd / "piskelapp.png")
-    assert img.shape() == img.data().shape
-    assert img.shape() == img.data().shape
-    img.plotAll()
+    img = ik.VxlImgU8(cwd / "piskelapp.png")
     print(img, img.data().shape)
+    img.printInfo()
+    assert img.shape() == img.data().shape
+    assert img.nz() == 1
+    img.plotAll(zProfile=False) # FIXME: zProfile hangs svplot when nz==1
+    img.distMapExtrude(offset=0.5, scale=2.0,)
+    assert img.nz() > 1
+    slice=8
+    img.plotAll(name="extruded", normalAxis="z", sliceIndex=slice)
+    assert Path(f"fig/extruded_z{slice}_grey.png").exists()
 
 if __name__ == "__main__":
     # test_version()