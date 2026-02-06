aliraeini/image3d

rename python package from voxlib to image3

Makefile
@@ -1,7 +1,7 @@
 build_update:
 	nox -s build
 	pip install .
-	pybind11-stubgen voxlib --output-dir src
+	pybind11-stubgen image3d --output-dir src
 
 install:
 	pip install .

src/image3d/__init__.pyi
@@ -11,13 +11,13 @@
     
 """
 from __future__ import annotations
-from voxlib._core import Input
-from voxlib._core import VxlImgU8
-from voxlib._core import cube
-from voxlib._core import cylinder
-from voxlib._core import readImage
-from voxlib._core import readImageU8
-from voxlib._core import sphere
+from image3d._core import Input
+from image3d._core import VxlImgU8
+from image3d._core import cube
+from image3d._core import cylinder
+from image3d._core import readImage
+from image3d._core import readImageU8
+from image3d._core import sphere
 from . import _core
 __all__: list = ['__doc__', '__version__', 'VxlImgU8', 'Input', 'sphere', 'cylinder', 'cube', 'readImage', 'readImageU8']
 __version__: str = '0.0.1'

DigiPorFlow/image3kit

Makefile
@@ -1,7 +1,7 @@
 
 build_update:
 	pip install .
-	pybind11-stubgen voxlib --output-dir src
+	pybind11-stubgen image3kit --output-dir src
 
 install:
 	pip install .

src/image3d/__init__.pyi
@@ -11,13 +11,13 @@
     
 """
 from __future__ import annotations
-from voxlib._core import Input
-from voxlib._core import VxlImgU8
-from voxlib._core import cube
-from voxlib._core import cylinder
-from voxlib._core import readImage
-from voxlib._core import readImageU8
-from voxlib._core import sphere
+from image3kit._core import Input
+from image3kit._core import VxlImgU8
+from image3kit._core import cube
+from image3kit._core import cylinder
+from image3kit._core import readImage
+from image3kit._core import readImageU8
+from image3kit._core import sphere
 from . import _core
 __all__: list = ['__doc__', '__version__', 'VxlImgU8', 'Input', 'sphere', 'cylinder', 'cube', 'readImage', 'readImageU8']
 __version__: str = '0.0.1'